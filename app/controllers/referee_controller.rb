03257f8228eeff95d1df60c1b4dc82b1bde5a1c8
robsonbraga <robsonbraga@8e027181-3025-0410-b61b-bbd32fa1d46d>
2007-07-30 14:38:34 +0000

    Added listing to referees.

--- app/controllers/referee_controller.rb
+++ app/controllers/referee_controller.rb
@@ -1,4 +1,27 @@
 class RefereeController < ApplicationController
   scaffold :referee
   before_filter :login_required, :except => [ :index, :list, :show ]
+
+  def index
+    redirect_to :action => :list
+  end
+
+  def list
+    items_per_page = 10
+    @name = params[:name]
+    conditions = ["name LIKE ?", "%#{@name}%"] unless @name.nil?
+
+    @total = Referee.count :conditions => conditions
+    @referee_pages, @referees = paginate(:referees, :order => "name",
+                                         :conditions => conditions,
+                                         :per_page => items_per_page)
+
+    if request.xhr?
+      render :partial => "referee_list", :layout => false
+    end
+  end
+
+  def show
+    @referee = Referee.find(params[:id])
+  end
 end
