7ffd69f760bed8c265ebccf17d837f25444526b1
robsonbraga <robsonbraga@8e027181-3025-0410-b61b-bbd32fa1d46d>
2007-08-01 21:34:11 +0000

    Use bigger pagination windows. Do not show score on scheduled games.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def pagination_links_remote(paginator, page_options={}, ajax_options={}, html_options={})
+  def pagination_links_remote(paginator, ajax_options={}, page_options={}, html_options={})
+    page_options = { :window_size => 8 }.merge(page_options)
     html = ""
     unless paginator.current.first?
       ajax_options[:url].merge!({:page => paginator.current.previous})
