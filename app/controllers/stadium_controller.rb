c56785a77ba6df7ff6533d5d7f7c22e420543e2e
Robson Araujo <robson@solaris-storage.(none)>
2008-04-11 00:09:44 -0300

    Fix stadium search

--- app/controllers/stadium_controller.rb
+++ app/controllers/stadium_controller.rb
@@ -7,25 +7,19 @@ class StadiumController < ApplicationController
   end
 
   def list
-    items_per_page = 10
     @name = params[:name]
     conditions = ["name LIKE ?", "%#{@name}%"] unless @name.nil?
 
-    @total = Stadium.count :conditions => conditions
-    @stadium_pages, @stadia = paginate(:stadia, :order => "name",
-                                       :conditions => conditions,
-                                       :per_page => items_per_page)
-
-    if request.xhr?
-      render :partial => "stadium_list", :layout => false
-    end
+    @stadia = Stadium.paginate :order => "name",
+                               :conditions => conditions,
+                               :page => params[:page]
   end
 
   def show
     store_location
     @stadium = Stadium.find(params[:id])
   end
-  
+
   def edit
     @stadium = Stadium.find(params[:id])
   end
