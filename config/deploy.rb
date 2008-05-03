a106ef5ddc8c1f703254c8b0b0d2bc16d3ea5765
Robson Araujo <robson@solaris-storage.(none)>
2008-05-02 22:56:00 -0300

    Upgraded to capistrano 2.2.0 and added patch to environment.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :user, "galo_2099"
-set :application, "golaberto"
 set :domain, "golaberto.com.br"
+set :application, "golaberto"
 
 set :deploy_to, "/home/galo_2099/#{application}"
 
@@ -8,8 +8,9 @@ set :deploy_to, "/home/galo_2099/#{application}"
 set :use_sudo, false
 
 set :scm, :git
-set :git, "/home/galo_2099/git/bin/git"
-set :repository,  "git://github.com/galo2099/golaberto.git"
+set :scm_command, "/home/galo_2099/git/bin/git"
+set :local_scm_command, "git"
+set :repository, "git://github.com/galo2099/golaberto.git"
 set :deploy_via, :remote_cache
 set :branch, "master"
 
@@ -30,7 +31,11 @@ task :after_update_code, :roles => :app do
   JS
   layout = "#{release_path}/app/views/layouts/application.rhtml"
   run "sed -i 's?</body>?#{stats}</body>?' #{layout}"
+
+  run "patch #{release_path}/config/environment.rb -i #{shared_path}/config/environment.patch"
+
   run "cp -f #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+
   run "ln -s #{shared_path}/logos #{release_path}/public/images/"
 end
 
