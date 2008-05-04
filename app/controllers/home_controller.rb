ac426c9d5403ae3c16291742f2f8cede40fe26bf
Robson Araujo <robson@solaris-storage.(none)>
2008-05-04 10:34:43 -0300

    Fix dates to show championships

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @today = Date.today
     @championships = Championship.find :all,
-        :conditions => [ "begin < ? AND end > ?", @today, @today ],
+        :conditions => [ "begin <= ? AND end >= ?", @today, @today ],
         :order => "category_id, begin"
     @comments = Comment.find :all, :order => "created_at DESC", :limit => 5
     @games = Game.find :all, :order => [ "updated_at DESC" ], :limit => 5
