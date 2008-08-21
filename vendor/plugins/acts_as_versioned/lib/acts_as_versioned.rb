7fd6d434cf1162b450012ad1c46f9cecf245242b
Robson Araujo <robson@solaris-storage.(none)>
2008-08-21 01:00:47 -0300

    Fix acts_as_versioned for a boolean attribute.

--- vendor/plugins/acts_as_versioned/lib/acts_as_versioned.rb
+++ vendor/plugins/acts_as_versioned/lib/acts_as_versioned.rb
@@ -333,7 +333,7 @@ module ActiveRecord #:nodoc:
         # Clones a model.  Used when saving a new version or reverting a model's version.
         def clone_versioned_model(orig_model, new_model)
           self.class.versioned_columns.each do |col|
-            new_model.send("#{col.name}=", orig_model.send(col.name)) if orig_model.has_attribute?(col.name)
+            new_model.send("#{col.name}=", orig_model.attributes_before_type_cast[col.name]) if orig_model.has_attribute?(col.name)
           end
 
           if orig_model.is_a?(self.class.versioned_class)
