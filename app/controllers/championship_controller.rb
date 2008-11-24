71fa28028078f64bc6b055629496077ecaa1ef6f
Robson Araujo <robson@solaris-storage.(none)>
2008-11-24 18:21:26 -0200

    Do not display odds if the phase is over

--- app/controllers/championship_controller.rb
+++ app/controllers/championship_controller.rb
@@ -42,6 +42,7 @@ class ChampionshipController < ApplicationController
   def phases
     @championship = Championship.find(params[:id])
     @current_phase = @championship.phases.find(params[:phase]) if params[:phase]
+    @display_odds = @current_phase.games.find_by_played(:first, false) == nil
   end
 
   def team_xml(championship, phase, group, team)
