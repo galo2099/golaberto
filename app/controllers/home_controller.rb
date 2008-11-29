91de2cf34fa91f0be30cc4b1563a40dbc3f4dabc
Robson Araujo <robson@solaris-storage.(none)>
2008-11-29 00:00:44 -0200

    Remove cookie from home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @cookies = cookies
     @today = Date.today
     @championships = Championship.find :all,
         :conditions => [ "begin <= ? AND end >= ?", @today, @today ],
