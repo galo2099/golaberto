b765b28bae58b0f29eac859c22157bb29c5e0d30
Robson Araujo <robson@solaris-storage.(none)>
2008-12-04 07:44:57 -0200

    Show more championships on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def index
     @today = Date.today
     @championships = Championship.find :all,
-        :conditions => [ "begin <= ? AND end >= ?", @today, @today ],
+        :conditions => [ "begin <= ? AND end >= ?", @today+30, @today-30 ],
         :order => "category_id, begin"
     @comments = Comment.find :all, :order => "created_at DESC", :limit => 5
     @games = Game.find :all, :order => [ "updated_at DESC" ], :limit => 5
