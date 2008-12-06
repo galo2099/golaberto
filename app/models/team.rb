da37fa065595c08027d21897077da63045784d73
Robson Araujo <robson@solaris-storage.(none)>
2008-12-06 11:14:17 -0200

    Fix crop image method.

--- app/models/team.rb
+++ app/models/team.rb
@@ -111,27 +111,26 @@ class Team < ActiveRecord::Base
     x_right = cols - 1
     y_top = 0
     y_bottom = rows - 1
-    transparent = Pixel.new(0, 0, 0, MaxRGB)
     cols.times do |i|
-      if image.get_pixels(i, 0, 1, rows).select{|p| p != transparent}.size > 0
+      if image.get_pixels(i, 0, 1, rows).select{|p| p.opacity != MaxRGB}.size > 0
         x_left = i
         break
       end
     end
     (cols - 1).downto(0) do |i|
-      if image.get_pixels(i, 0, 1, rows).select{|p| p != transparent}.size > 0
+      if image.get_pixels(i, 0, 1, rows).select{|p| p.opacity != MaxRGB}.size > 0
         x_right = i + 1
         break
       end
     end
     rows.times do |i|
-      if image.get_pixels(0, i, cols, 1).select{|p| p != transparent}.size > 0
+      if image.get_pixels(0, i, cols, 1).select{|p| p.opacity != MaxRGB}.size > 0
         y_top = i
         break
       end
     end
     (rows - 1).downto(0) do |i|
-      if image.get_pixels(0, i, cols, 1).select{|p| p != transparent}.size > 0
+      if image.get_pixels(0, i, cols, 1).select{|p| p.opacity != MaxRGB}.size > 0
         y_bottom = i + 1
         break
       end
@@ -141,7 +140,7 @@ class Team < ActiveRecord::Base
     y_top = y_bottom if y_top > y_bottom
     image.crop(x_left, y_top, x_right - x_left, y_bottom - y_top)
   end
-  
+
   def remove_background(image)
     image.fuzz = "5%"
     cols = image.columns
