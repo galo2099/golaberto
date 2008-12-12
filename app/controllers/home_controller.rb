037414c9ef3781f18de08ea18073bf9340d34595
Robson Araujo <robson@solaris-storage.(none)>
2008-12-12 13:54:02 -0200

    Order the championships by name

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @today = Date.today
     @championships = Championship.find :all,
         :conditions => [ "begin <= ? AND end >= ?", @today+30, @today-30 ],
-        :order => "category_id, begin"
+        :order => "category_id, name, begin"
     @comments = Comment.find :all, :order => "created_at DESC", :limit => 5
     @games = Game.find :all, :order => [ "updated_at DESC" ], :limit => 5
   end
