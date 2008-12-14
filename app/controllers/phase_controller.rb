5981b33ed440aa151702a4b156e51ffe1620ee88
Robson Araujo <robson@solaris-storage.(none)>
2008-12-14 10:19:46 -0200

    Add more groups at once

--- app/controllers/phase_controller.rb
+++ app/controllers/phase_controller.rb
@@ -21,6 +21,20 @@ class PhaseController < ApplicationController
     @phase = Phase.find(params["id"])
   end
 
+  def add_groups
+    phase = Phase.find(params["id"])
+    last_group = phase.groups[-1]
+    4.times do
+      new_name = returning last_group.name.split(" ") do |x|
+        x[1].succ!
+      end.join " "
+      last_group = phase.groups.build
+      last_group.name = new_name
+      last_group.save!
+    end
+    render :partial => "group_list", :object => phase
+  end
+
   def update
     @phase = Phase.find(params["id"])
     @phase.attributes = params["phase"]
