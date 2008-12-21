24d8aa0eef94f2298cf26c98ca681ca0646d5424
Robson Araujo <robson@solaris-storage.(none)>
2008-12-20 23:05:47 -0200

    Only show updates made by actual users.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,6 @@ class HomeController < ApplicationController
         :conditions => [ "begin <= ? AND end >= ?", @today+30, @today-30 ],
         :order => "category_id, name, begin"
     @comments = Comment.find :all, :order => "created_at DESC", :limit => 5
-    @games = Game.find :all, :order => [ "updated_at DESC" ], :limit => 5
+    @games = Game.find :all, :conditions => "updater_id != 0", :order => [ "updated_at DESC" ], :limit => 5
   end
 end
