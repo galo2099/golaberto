2a8064706b8285ea78075ae377241a611e66b044
Robson Araujo <robson@solaris-storage.(none)>
2008-12-23 17:23:01 -0200

    Implement list view for users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,14 +4,15 @@ class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
     @comment_number = @user.comments.count
-    @last_comments = @user.comments.find(:all, :order => "created_at DESC", :limit => 5)
     @edit_number = @user.game_edits.count
-    @last_edits = @user.game_edits.find(:all, :order => "updated_at DESC", :limit => 5)
   end
 
   def list
-    @users = User.paginate :order => "name, login, identity_url",
-                           :page => params[:page]
+    @users = User.paginate(
+        :select => "*, (select count(game_versions.id) from game_versions where game_versions.updater_id = users.id) edit_count, " +
+                   "(select count(comments.id) from comments where `comments`.user_id = users.id) comment_count",
+        :order => "edit_count DESC, comment_count DESC, name, login, identity_url",
+        :page => params[:page])
   end
 
   def edit
