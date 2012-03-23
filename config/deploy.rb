6678ba46ec8f388390b12f2c8cc023b2899c0c08
Robson Braga Araujo <robsonbraga@gmail.com>
2012-03-23 04:38:16 -0300

    Fix analytics.

--- config/deploy.rb
+++ config/deploy.rb
@@ -79,16 +79,16 @@ namespace :deploy do
     stats = <<-JS
       <script type="text/javascript">
         var _gaq = _gaq || [];
-        _gaq.push(['_setAccount', 'UA-1911106-4']);
-        _gaq.push(['_trackPageview']);
+        _gaq.push(["_setAccount", "UA-1911106-4"]);
+        _gaq.push(["_trackPageview"]);
         (function() {
-          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
+          ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+          var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
         })();
       </script>
     JS
-    layout = "#{current_path}/app/views/layouts/application.html.erb"
+    layout = "#{release_path}/app/views/layouts/application.html.erb"
     run "sed -i 's#</body>##{stats}</body>#' #{layout}"
   end
 end
