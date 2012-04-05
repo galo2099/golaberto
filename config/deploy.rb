423eec696083818a8806ff8bb05fffcd725662d8
Robson Braga Araujo <robsonbraga@gmail.com>
2012-04-04 21:09:17 -0700

    Set GA site speed sampling rate to 100 percent.

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,6 +80,7 @@ namespace :deploy do
       <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(["_setAccount", "UA-1911106-4"]);
+        _gaq.push(["_setSiteSpeedSampleRate", 100]);
         _gaq.push(["_trackPageview"]);
         (function() {
           var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;
