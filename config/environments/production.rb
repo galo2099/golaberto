3f6c2b802510455b59e73519773e8587fc167898
Robson Braga Araujo <robsonbraga@gmail.com>
2013-09-13 01:15:44 -0700

    Fix for bg image and openid javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,9 @@ Golaberto::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
+  # Loads all registered config.eager_load_namespaces.
+  config.eager_load = true
+
   # Enable threaded mode
   # config.threadsafe!
 
