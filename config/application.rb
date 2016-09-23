f2f3870728d3dcf53a817775255a0646382a2441
Robson Braga Araujo <robsonbraga@gmail.com>
2016-09-22 22:41:02 -0700

    add config.active_record.raise_in_transactional_callbacks to avoid warnings

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Golaberto
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
