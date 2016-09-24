4792e0eb11f016b645145fa181d1521ed2417acf
Robson Braga Araujo <robsonbraga@gmail.com>
2016-09-24 04:05:06 -0700

    Show top games in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,13 @@ class HomeController < ApplicationController
   authorize_resource :class => false
 
   def index
-    @today = cookie_timezone.today
-    @championships = Championship.where("begin <= ? AND end >= ?", @today+30, @today-30).
+    today = cookie_timezone.today
+    @championships = Championship.where("begin <= ? AND end >= ?", today+30, today-30).
         order("category_id, name, begin").includes(phases: :teams)
-    @championships = @championships.all.sort{|b,a| a.avg_team_rating <=> b.avg_team_rating}
+    @championships = @championships.all.sort_by{|a| -a.avg_team_rating }
     @comments = Comment.order("created_at DESC").limit(5)
     @games = Game.where("updater_id != 0").order("updated_at DESC").limit(5)
+    @top_games = Game.joins(:home, :away).select("games.*, 2*teams.rating*aways_games.rating/(teams.rating+aways_games.rating) as quality").order("quality desc, date desc").where(played: false).where("date > ?", Time.now - 3.hours).where("date < ?", Time.now + 1.day - 3.hours).limit(20)
+    @top_games = @top_games.to_a.sort_by{|a|[a.date.in_time_zone(cookie_timezone).to_date, -a.phase.avg_team_rating, a.date]}
   end
 end
