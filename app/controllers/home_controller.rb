3a0b57c41f31f5d20ca7779a5b60934aa3c69e20
Robson Araujo <robson@snap.com>
2017-06-14 02:28:38 -0700

    Improve home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,7 @@ class HomeController < ApplicationController
     @championships = Championship.where("begin <= ? AND end >= ?", today+30, today-30).
         order("category_id, name, begin").includes(phases: :teams)
     @championships = @championships.all.sort_by{|a| -a.avg_team_rating }
-    @continents = []
-    @championships.each do |c|
-      continent = ApplicationHelper::Continent.country_to_continent[c.teams[0].country]
-      if not @continents.include? continent
-        @continents << continent
-      end
-    end
+    @regions = Championship.regions.keys
     @comments = Comment.order("created_at DESC").limit(5)
     @games = Game.where("updater_id != 0").order("updated_at DESC").limit(5)
     @top_games = Game.joins(:home, :away).select("games.*, 2*teams.rating*aways_games.rating/(teams.rating+aways_games.rating) as quality").order("quality desc, date desc").where(played: false).where("date > ?", Time.now - 3.hours).where("date < ?", Time.now + 1.day - 3.hours).where(has_time: true).limit(20)
