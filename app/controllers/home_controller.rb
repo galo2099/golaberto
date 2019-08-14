12dfe268417d5a418d704a23d5e2aaeb783926c4
Robson Araujo <robsonbraga@gmail.com>
2019-08-13 22:17:40 -0700

    use 0 if importance is null

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
     @regions = Championship.regions.keys
     @comments = Comment.includes(:user).order("created_at DESC").limit(5)
     @games = Game.includes(:home, :away, :updater, {phase: :championship}).where("updater_id != 0").order(updated_at: :desc).limit(5)
-    @top_games = Game.includes(:home, :away).select("games.*, 2*teams.rating*aways_games.rating/(teams.rating+aways_games.rating)*(1+(home_importance+away_importance)/2) as quality").order("quality desc, date desc").where(played: false).where("date > ?", Time.now - 3.hours).where("date < ?", Time.now + 1.day - 3.hours).where(has_time: true).limit(20).references(:home, :away)
+    @top_games = Game.includes(:home, :away).select("games.*, 2*teams.rating*aways_games.rating/(teams.rating+aways_games.rating)*(1+(IFNULL(home_importance,0)+IFNULL(away_importance,0))/2) as quality").order("quality desc, date desc").where(played: false).where("date > ?", Time.now - 3.hours).where("date < ?", Time.now + 1.day - 3.hours).where(has_time: true).limit(20).references(:home, :away)
     top_quality_championship = {}
     @top_games.each do |g|
       top_quality_championship[g.phase.id] = g.game_quality if g.game_quality > top_quality_championship[g.phase.id].to_f
