813f40e731e5c607d96c6a0b37905de7fff3bfcc
Robson Araujo <robsonbraga@gmail.com>
2019-10-03 12:20:36 -0700

    Add zooming support to team ratings

---
+++ app/assets/javascripts/jquery.flot.all.js
@@ -0,0 +1,6 @@
+//= require jquery.flot
+//= require jquery.flot
+//= require jquery.flot.saturated
+//= require jquery.flot.time
+//= require jquery.flot.uiConstants
+//= require jquery.flot.selection
