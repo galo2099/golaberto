3cadcdad685da5f6bb3be1bd8bc82acfe6654bd6
Robson Araujo <robson@meemo.ai>
2020-11-02 13:30:32 -0800

    better scraping

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -188,21 +188,22 @@ def fix_name(str)
   if str == "JBL Zulia"
     return "Deportivo JBL"
   end
+  if str == "Unión"
+    return "Unión de Santa Fe"
+  end
   str
 end
 
 def rounds_to_update(phase)
-  (phase.games.where(date: (Date.today..Date.today+30.days)).group(:round).size.keys +
+  (phase.games.where(date: (Date.today-10.days..Date.today+30.days)).group(:round).size.keys +
    phase.games.where(played: false).where("date < ?", Time.now).map{|g|g.round} +
    phase.games.where(played: true).includes(:player_games).select{|g|g.player_games.size == 0}.map{|g|g.round}).sort.uniq
 end
 
 def scrape(phase, round_id, competition_id, options = {})
   phase = Phase.find phase
-  fuzzy_match = FuzzyTeamMatch.new
 
   altered = false
-  count = 0
   round = 0
   date = Date.today
   rounds = nil
@@ -212,48 +213,60 @@ def scrape(phase, round_id, competition_id, options = {})
     rounds = rounds_to_update(phase)
   end
   rounds.each do |i|
+    puts "Round #{i}"
     ChampionshipGet.matches(round_id, competition_id, i).each do |match|
-      round = i
-      datetime = Time.at(match.attributes["data-timestamp"].to_i).to_datetime.in_time_zone("UTC")
-      home_name = fix_name(match.search("td[2]//text()").to_s)
-      away_name = fix_name(match.search("td[4]//text()").to_s)
-      home = phase.teams.map{|t| [t, fuzzy_match.getDistance(t.name, home_name)]}.sort{|a,b|b[1] <=> a[1]}[0][0]
-      away = phase.teams.map{|t| [t, fuzzy_match.getDistance(t.name, away_name)]}.sort{|a,b|b[1] <=> a[1]}[0][0]
-      p "#{home_name} #{home.name}"
-      p "#{away_name} #{away.name}"
-      g = phase.games.where(:home_id => home.id, :away_id => away.id, :round => round).includes(:goals).first
-      unless g
-        g = phase.games.build({:home_id => home.id, :away_id => away.id})
-      end
-      if g
-        game_compare = g.dup
-        game_compare.goals = g.goals
-        g.date = datetime
-        g.has_time = true
-        g.round = round
-        g.played = "0"
-        if match.search("td[3]//text()").to_s =~ /PSTP/
-          g.has_time = false
-        end
-        if match.search("td[3]//text()").to_s =~ /(\d+) - (\d+)/
-          g.home_score = $1.to_i
-          g.away_score = $2.to_i
-          g.played = true
-        end
-        if match.search("td[1]//text()").to_s !~ /FT/
-          g.played = false
-        end
-        if g.diff(game_compare).size > 0
-          p g
-          p game_compare.diff(g)
-          g.valid? || raise(g.errors.to_xml.to_s)
-          altered = g.save! || altered
-        end
-        if g.played and g.player_games.size == 0
-          get_scorers(g, "https://us.soccerway.com" + match.search("td[3]/a").first.attributes["href"])
-        end
-      end
-      count = count + 1
+      parse_match(phase, i, match)
+    end
+  end
+end
+
+def parse_match(phase, round, match)
+  fuzzy_match = FuzzyTeamMatch.new
+  datetime = Time.at(match.attributes["data-timestamp"].to_i).to_datetime.in_time_zone("UTC")
+  home_name = fix_name(match.search("td[2]//text()").to_s.gsub(/^\s+/, "").gsub(/\s+$/, ""))
+  away_name = fix_name(match.search("td[4]//text()").to_s.gsub(/^\s+/, "").gsub(/\s+$/, ""))
+  home = phase.teams.map{|t| [t, fuzzy_match.getDistance(t.name, home_name)]}.sort{|a,b|b[1] <=> a[1]}[0][0]
+  away = phase.teams.map{|t| [t, fuzzy_match.getDistance(t.name, away_name)]}.sort{|a,b|b[1] <=> a[1]}[0][0]
+  p "#{home_name} #{home.name}"
+  p "#{away_name} #{away.name}"
+  g = nil
+  soccerway_url = match.search("td[3]/a").first.attributes["href"]
+  soccerway_id = soccerway_url.gsub(/.*?(\d+).$/, '\1')
+  g = phase.games.where(soccerway_id: soccerway_id).includes(:goals).first
+  # legacy
+  unless g
+    g = phase.games.where(home_id: home.id, away_id: away.id, round: round, soccerway_id: nil).includes(:goals).first
+  end
+  unless g
+    g = phase.games.build({:home_id => home.id, :away_id => away.id})
+  end
+  if g
+    game_compare = g.dup
+    game_compare.goals = g.goals
+    g.date = datetime
+    g.soccerway_id = soccerway_id
+    g.has_time = true
+    g.round = round
+    g.played = "0"
+    if match.search("td[3]//text()").to_s =~ /PSTP/
+      g.has_time = false
+    end
+    if match.search("td[3]//text()").to_s =~ /(\d+) - (\d+)/
+      g.home_score = $1.to_i
+      g.away_score = $2.to_i
+      g.played = true
+    end
+    if match.search("td[1]//text()").to_s !~ /FT/
+      g.played = false
+    end
+    if g.diff(game_compare).size > 0
+      p g
+      p game_compare.diff(g)
+      g.valid? || raise(g.errors.to_xml.to_s)
+      altered = g.save! || altered
+    end
+    if g.played
+      get_scorers(g, "https://us.soccerway.com" + match.search("td[3]/a").first.attributes["href"])
     end
   end
 end
