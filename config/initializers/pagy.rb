7790835c81751e403b65d58b9ae50c59b48bedc5
galo2099 <robsonbraga@gmail.com>
2022-12-19 20:47:36 -0800

    missing file

---
+++ config/initializers/pagy.rb
@@ -0,0 +1,5 @@
+require 'pagy/extras/calendar'
+require 'pagy/extras/i18n'
+#require 'pagy/extras/overflow'
+# default :empty_page (other options :last_page and :exception )
+#Pagy::DEFAULT[:overflow] = :empty_page
