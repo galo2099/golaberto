03414488b375cb524f0c2ff2930d5a8f95f84bbf
galo2099 <robsonbraga@gmail.com>
2024-01-23 11:13:51 -0800

    Update libraries to work with Ruby 3.2

---
+++ config/initializers/uri_escape.rb
@@ -0,0 +1,21 @@
+require 'uri'
+
+module URI
+    class << self
+        def escape(str)
+            alpha = "a-zA-Z"
+            alnum = "#{alpha}\\d"
+            unreserved = "\\-_.!~*'()#{alnum}"
+            reserved = ";/?:@&=+$,\\[\\]"
+            unsafe = Regexp.new("[^#{unreserved}#{reserved}]")
+            str.gsub(unsafe) do
+                us = $&
+                tmp = ''
+                us.each_byte do |uc|
+                    tmp << sprintf('%%%02X', uc)
+                end
+                tmp
+            end.force_encoding(Encoding::US_ASCII)
+        end
+    end
+end
