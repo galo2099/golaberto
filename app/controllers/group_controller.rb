ae35c461fec72eed4fbd7339e3787e80cbb2c63b
galo2099 <robsonbraga@gmail.com>
2026-02-17 14:22:07 -0800

    Always reset odds history on championship backfill

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -82,14 +82,12 @@ class GroupController < ApplicationController
     phase_id = params["phase"].presence
     from_date = params["from"].present? ? Date.parse(params["from"]) : nil
     to_date = params["to"].present? ? Date.parse(params["to"]) : nil
-    reset = params["reset"].to_s == "true"
-
     @backfill_started = OddsHistoryBackfillService.start_async(
       championship_id: championship_id,
       phase_id: phase_id,
       from_date: from_date,
       to_date: to_date,
-      reset: reset,
+      reset: true,
     ) == :started
   rescue ArgumentError
     @backfill_started = false
