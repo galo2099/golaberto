d8b18282e3e9593db0cfee540f2982a4ec5e55dd
galo2099 <robsonbraga@gmail.com>
2026-02-23 22:30:48 -0800

    Extract Rufus scheduler into dedicated daemon binary

---
+++ lib/rufus_scheduler_runner.rb
@@ -0,0 +1,32 @@
+class RufusSchedulerRunner
+  def self.start
+    new.start
+  end
+
+  def start
+    scheduler.every '1h', first_in: '5m' do
+      refetch = Time.current.hour == 3
+      Rails.logger.info "[scheduler] Starting periodic game data scrape (refetch=#{refetch})"
+      result = GameDataScrapeService.start_async(refetch: refetch)
+      Rails.logger.info "[scheduler] Game data scrape result: #{result}"
+    end
+
+    scheduler.cron '30 4 * * *' do
+      Rails.logger.info '[scheduler] Starting daily player ratings update'
+      begin
+        response = PlayerRatingUpdateService.run
+        Rails.logger.info "[scheduler] Player ratings update finished with status #{response.code}"
+      rescue => e
+        Rails.logger.error "[scheduler] ERROR updating player ratings: #{e.class}: #{e.message}"
+      end
+    end
+
+    scheduler.join
+  end
+
+  private
+
+  def scheduler
+    @scheduler ||= Rufus::Scheduler.new
+  end
+end
